f15b4b0aca613d0b2a8086b5c945290b559224ee
balqishafiz <49114676+balqishafiz@users.noreply.github.com>
2019-03-31 15:30:42 +0800

    Add files via upload
    Simple script

---
+++ ρяσgσαℓ_simple_script.lua
@@ -0,0 +1,499 @@
+gg.alert('☞ ғᴏʀ ᴍᴏᴅ ʙʏᴘᴀꜱꜱ ᴏɴʟʏ ☜')
+gg.alert('ᴍᴀᴅᴇ ʙʏ  🇵 🇷 🇴 🇬 🇴 🇦 🇱 ')
+print('🎮 ᴄᴏᴍᴇ ᴘʟᴀʏ 🎮')
+
+gg.toast("ɪғ ꜱᴄᴀʀᴇ,ᴅᴏɴᴛ ᴘʟᴀʏ ᴡɪᴛʜ ᴄʜᴇᴀᴛ")
+
+
+
+
+
+HOME = 1
+
+function HOME()
+AC = gg.choice({
+"➥ ᴡᴀʟʟʜᴀᴄᴋ 🄾🄽 ",
+"➥ ᴡᴀʟʟʜᴀᴄᴋ 🅾🅵🅵",
+"➥ ʙᴏᴅy ʙʟᴀᴄᴋ",
+"➥ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴍᴍᴏ",
+"➥ ς๏l๏г ғʟᴏᴏʀ",
+"➥  ᴄʜᴀɴɢᴇ ɢʀᴏᴜɴᴅ",
+"➥ 1 ʜɪᴛ ᴡᴇᴀᴘᴏɴ  🅼🅴🅽🆄",
+"➥ ʙʏᴘᴀss 🅼🅴🅽🆄  ☀ ᴀᴄᴛɪᴠᴀᴛᴇ ʙᴇғᴏʀᴇ ʟᴏɢɪɴ",
+"➥ ꜰᴩꜱ ʜꜱ 🅼🅴🅽🆄",
+
+"🇪 🇽 🇮 🇹 "},
+nil,
+											os.date ("🅓🅐🅣🅔 :- %A, %d %B %Y 🅣🅘🅜🅔 :- %H:%M:%S\nScript Prepared By : 🄿🅁🄾🄶🄾🄰🄻\nsᴜʙsᴄʀɪʙᴇ ᴍʏ ʙᴇsᴛɪᴇ:: 🇬 🇦 🇲 🇪 🇷 🇿 🇹 🇻 "))
+
+
+
+
+if AC == 1 then WHON() end
+if AC == 2 then WHOFF() end
+if AC == 3 then BC() end
+if AC == 4 then BC() end
+if AC == 5 then BL() end
+if AC == 6 then CG() end
+if AC == 7 then HIT() end
+if AC == 8 then BY() end
+if AC == 9 then FPS() end
+if AC == 10 then EXIT() end
+HOMEDM=-1
+end
+
+function WHON()
+if menu == 2 then WHON() end
+HOMEDM=-1
+end
+
+function WHON()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_BSS) gg.searchNumber("9.9999997e-10", gg.TYPE_FLOAT) gg.getResults(7604) gg.editAll("3.4e38", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("🇼 🇦 🇱 🇱 🇭 🇦 🇨 🇰")
+end
+
+
+function WHOFF()
+if menu == 2 then WHOFF() end
+HOMEDM=-1
+end
+
+function WHOFF()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_BSS)
+gg.searchNumber("3.4e38", gg.TYPE_FLOAT)
+gg.getResults(7604)
+gg.editAll("9.9999997e-10", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("🇼 🇦 🇱 🇱 🇭 🇦 🇨 🇰 ")
+end
+
+
+
+function BC()
+if menu == 2 then BC() end
+HOMEDM=-1
+end
+
+function BC()
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("1;31,576,666.0;-1.6767658e32", gg.TYPE_FLOAT)
+gg.searchNumber("1", gg.TYPE_FLOAT)
+gg.getResults(4)
+gg.editAll("999", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("🇧 🇱 🇦 🇨 🇰 ")
+end
+
+
+
+function UA()
+if menu == 2 then UA() end
+HOMEDM=-1
+end
+
+function UA()
+gg.getRanges()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("999", gg.TYPE_DOUBLE)
+gg.searchNumber("1,072,064,102;1.0E;1,072,693,248;1;1,072,693,248;1,071,644,672::100", gg.TYPE_DWORD)
+gg.getResults(10)
+gg.toast(" CODE DEEPLY EXECUTE... ")
+gg.getRanges()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1,717,986,918;1,072,064,102;1.0E;1,072,693,248;1;1,072,693,248::37", gg.TYPE_DWORD)
+gg.getResults(10)
+gg.toast(" CODE DEEPLY EXECUTE... ")
+gg.getRanges()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1,076,101,120;1,717,986,918;1,072,064,102;1.0E;1,072,693,248;1;1,072,693,248::129", gg.TYPE_DWORD)
+gg.getResults(4)
+gg.searchNumber("1", gg.TYPE_DOUBLE)
+gg.getResults(6)
+gg.toast("⭕ UNLI AMMO ⭕")
+gg.editAll("9999", gg.TYPE_DOUBLE)
+gg.alert("⭕ UNLIMITED CAPACITY AMMO ⭕")
+end
+
+
+
+
+
+function BL()
+if menu == 2 then BL() end
+HOMEDM=-1
+end
+
+function BL()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("537,152,000D;8D;1,074,794,499D;1,081,081,856D", gg.TYPE_DWORD)
+gg.searchNumber("8", gg.TYPE_DWORD)
+gg.getResults(15)
+gg.editAll("7", gg.TYPE_DWORD)
+gg.toast("🇨 🇴 🇱 🇴 🇷 🇫 🇱 🇴 🇴 🇷 ")
+gg.clearResults()
+end
+
+
+
+function CG()
+if menu == 2 then CG() end
+HOMEDM=-1
+end
+
+function CG()
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("1.250", gg.TYPE_FLOAT)
+gg.getResults(100)
+gg.editAll("10.8", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("🇬 🇷 🇴 🇺 🇳 🇩 ")
+end
+
+
+function HIT()
+if menu == 2 then HIT() end
+HOMEDM=-1
+end
+
+function HIT()
+  SN = gg.multiChoice({
+  "  AKM  💨",
+  "  MP5  💨",
+  "  QBZ 💨 ",
+  "  AN 94  💨",
+  "  EBR  💨",
+  "  P90  💨",
+  "  VECTOR  💨",
+  "  M4A1  💨",
+  "  THOMPSON  💨",
+  "  PP19  💨",
+  "  P90  💨",
+  "  AR15  💨",
+  "➥ ʜᴏᴍᴇ"
+ }, nil, "✘ sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ 🄿🅁🄾🄶🄾🄰🄻 ✘")
+  if SN == nil then
+  else
+  if SN[1] == true then
+   akm()
+  end
+  if SN[2] == true then
+   mp5()
+  end
+  if SN[3] == true then
+   qbz()
+  end
+  if SN[4] == true then
+   an94()
+  end
+  if SN[5] == true then
+    ebr()
+  end
+  if SN[6] == true then
+   dsr()
+  end
+  if SN[7] == true then
+   vec()
+  end
+  if SN[8] == true then
+   m4()
+  end
+  if SN[9] == true then
+   tom()
+   end
+  if SN[10] == true then
+   p19()
+  end
+  if SN[11] == true then
+   p90()
+  end
+  if SN[12] == true then
+     ar()
+  end
+  if SN[13] == true then
+   HOME()
+  end
+end
+  XGCK = -1
+end
+
+function akm()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("20,002D;90,056D;112,194D:512", gg.TYPE_DWORD)
+gg.searchNumber("20002", gg.TYPE_DWORD)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇦 🇰 🇲  ")
+end
+
+function mp5()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("95,925;20,025;95,923:73", gg.TYPE_DWORD)
+gg.searchNumber("20025", gg.TYPE_DWORD)
+gg.getResults(1)
+gg.editAll(20004, gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇲 🇵 5 ")
+end
+
+function qbz()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("112,107D;20,042D:512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20042", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇶 🇧 🇿 ")
+end
+
+function an94()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("112,824D;112,825D;20,024D:512", gg.TYPE_DWORD)
+gg.searchNumber("20024", gg.TYPE_DWORD)
+gg.getResults(2)
+gg.editAll(20004, gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇦 🇳 94")
+end
+
+function ebr()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber("5845D;20009D;2267:512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20009", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇲 14🇪 🇧 🇷 ")
+end
+
+function dsr()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("112775D;20034D;112774D:37", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20034", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇩 🇸 🇷  ")
+end
+
+function vec()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber("2,302;20,017;2,301", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20017", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇻 🇪 🇨 🇹 🇴 🇷 ")
+end
+
+function m4()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("90,052;20,001;90,053:61", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20001", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇲 4🇦 1 ")
+end
+
+function tom()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber("112,924;20,013;112,873", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20013", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇹 🇭 🇴 🇲 🇵 🇸 🇴 🇳 ")
+end
+
+function p19()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("20,012;5,135;5,035:49", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20012", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇵 🇵 19")
+end
+
+function p90()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber("21000;20018D;95886D:512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20018", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.toast("🇵 90")
+end
+
+function ar()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("20,010D;22,001D;112,944D:512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("20010", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("20004", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("🇦 🇷 15")
+end
+
+
+
+function BY()
+if menu == 2 then BY() end
+HOMEDM=-1
+end
+
+function BY()
+  SN = gg.multiChoice({
+  "➥ ɢɪᴀɴᴛ ᴍᴏᴅᴇ 1",
+  "➥ ɴᴏ ʀᴇᴄᴏɪʟ",
+  "➥ ɢɪᴀɴᴛ ᴍᴏᴅᴇ 2",
+  "➥ ʜᴏᴍᴇ"
+ }, nil, "✘ sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ 🄿🅁🄾🄶🄾🄰🄻 ✘")
+  if SN == nil then
+  else
+  if SN[1] == true then
+   g()
+  end
+  if SN[2] == true then
+   nr1h()
+   end
+  if SN[3] == true then
+   g2()
+  end
+  if SN[4] == true then
+   HOME()
+  end
+end
+  XGCK = -1
+end
+
+
+
+
+function g()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC)
+gg.searchNumber("5.21044921875F;1,085,180,928D;5.9765625F;1,073,217,536D;2.375F;2.625F;1:512", gg.TYPE_DOUBLE)
+gg.searchNumber("1.0", gg.TYPE_DOUBLE)
+gg.getResults(3)
+gg.editAll("3.5", gg.TYPE_DOUBLE)
+gg.clearResults()
+gg.toast("🇬 🇮 🇦 🇳 🇹 ")
+end
+
+
+function nr1h()
+gg.getRanges()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_ANONYMOUS)
+gg.searchNumber("1.23E;1.0E;400.0E;1.5E;1.9375F", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.0", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+local t = gg.getResults(4)
+gg.editAll("0", gg.TYPE_DOUBLE)
+gg.toast("🇳 🇴   🇷 🇪 🇨 🇴 🇮 🇱 ")
+end
+
+function g2()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1.0E;3,910.0E;3,000.0E;300.0E;100.0E;2,910.0E:512", gg.TYPE_DOUBLE)
+gg.searchNumber("1.0", gg.TYPE_DOUBLE)
+gg.getResults(2)
+gg.editAll("4.0", gg.TYPE_DOUBLE)
+gg.clearResults()
+gg.toast("🇬 🇮 🇦 🇳 🇹 ")
+end
+
+
+function FPS()
+if menu == 2 then FPS() end
+HOMEDM=-1
+end
+
+function FPS()
+  SN = gg.multiChoice({
+  "➥ hs fps prone",
+  "➥ hs fps stand",
+  "➥ ʜᴏᴍᴇ"
+ }, nil, "✘ sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ 🄿🅁🄾🄶🄾🄰🄻 ✘")
+  if SN == nil then
+  else
+  if SN[1] == true then
+   p()
+  end
+  if SN[2] == true then
+   s()
+  end
+  if SN[3] == true then
+   HOME()
+  end
+end
+  XGCK = -1
+end
+
+
+
+function p()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP | gg.REGION_C_ALLOC)
+gg.searchNumber("3.79999995232F;-1,069,547,520D:5", gg.TYPE_FLOAT)
+gg.searchNumber("3.79999995232", gg.TYPE_FLOAT)
+gg.getResults(1)
+gg.editAll("80.0", gg.TYPE_FLOAT)
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_CODE_APP | gg.REGION_C_ALLOC)
+gg.clearResults()
+gg.toast("🇵 🇷 🇴 🇳 🇪 ")
+end
+
+
+
+function s()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC | gg.REGION_CODE_APP)
+gg.searchNumber("20.0F;-1,069,547,520D:5", gg.TYPE_FLOAT)
+gg.searchNumber("20.0", gg.TYPE_FLOAT)
+gg.getResults(1)
+gg.editAll("80.0", gg.TYPE_FLOAT)
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_C_ALLOC | gg.REGION_CODE_APP)
+gg.clearResults()
+gg.toast("🇸 🇹 🇦 🇳 🇩 ")
+end
+
+
+
+
+function EXIT()
+if menu == 10 then EXIT() end
+HOMEDM=-1
+end
+
+function EXIT()
+    print('ᴘʀᴏɢᴏᴀʟ™')
+    print('1st SCRIPT FROM ME')
+    print('ʙᴀʟǫɪsʜᴀғɪᴢ')
+  os.exit()
+end
+
+while(true)
+do
+  if gg.isVisible(true) then
+    HOMEDM = 1
+    gg.setVisible(false) 
+  end 
+  if HOMEDM == 1 then HOME() end
+end
